4a025ec297d901195b34935e95db7f28862856cc
Pavel <bejenar@list.ru>
2023-01-22 20:33:43 +0300

    Add Homejob

---
+++ Homejob/Homejob_001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+    while(true)
+    {
+        string input = Console.ReadLine();
+
+        Console.WriteLine("{0}->{1}",input, input[1]);
+        break;
+    }
---
+++ Homejob/Homejob_003/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp3 
+{ 
+    internal class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            while (true) 
+            { 
+                Console.Write("\n Введите номер дня недели:  "); 
+                int num = int.Parse(Console.ReadLine()); 
+                if (num >= 1 && num <= 5) 
+                { 
+                    Console.Write(" Нет "); 
+                } 
+ 
+                else if (num >= 6 && num <= 7) 
+                { 
+                    Console.Write(" Да "); 
+                } 
+                else Console.Write(" Нет "); 
+                break;
+            } 
+        } 
+    } 
+}
